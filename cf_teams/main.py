2c0e9640386749139c62d12d0af4a24b1a51ec23
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2024-08-14 20:48:24 +0200

    Added the license header and the python3 default shebang
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- cf_teams/main.py
+++ cf_teams/main.py
@@ -1,3 +1,41 @@
+#!/usr/bin/python3
+################################################################################
+#
+# Cloudflare WARP GUI for linux
+#
+# (C) 2022, Pham Ngoc Son <phamngocsonls@gmail.com> - Public Domain
+# (C) 2024, Roberto A. Foglietta <roberto.foglietta@gmail.com> - 3-clause BSD
+#
+################################################################################
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its contributors
+#    may be used to endorse or promote products derived from this software
+#    without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+# THE POSSIBILITY OF SUCH DAMAGE.
+#
+################################################################################
+
 # Import Module
 from tkinter import *
 import subprocess
@@ -55,7 +93,6 @@ def cf_info():
     return version
     
 def get_status():
-
     status = subprocess.getoutput("warp-cli status")
     if status.find("Status update: Disconnected") > -1:
         status = False
@@ -79,18 +116,17 @@ def get_ip():
         details = handler.getDetails(ipdis).country
         return ipdis + " (" + details + ")"
     except:
-        try:
+    try:
             ipdis = get('https://ifconfig.me/',timeout= 2.5).text
             details = handler.getDetails(ipdis).country
-            return ipdis + " (" + details + ")"
-        except:
-            return ipdis
+        return ipdis + " (" + details + ")"
+    except:
+        return ipdis
 
 def enroll():
     if get_acc_type() == True:
         subprocess.Popen("yes yes | warp-cli register", shell=True)
-        slogan.config(image = logo)
-        
+        slogan.config(image = logo)  
         subprocess.getoutput("warp-cli disconnect")
 
     else:
@@ -154,7 +190,7 @@ lbl.place(relx=0.0, rely=1.0, anchor='sw')
 # Keep track of the button state on/off
 #global is_on
 is_on = False
-  
+
 
 #status label
 try:
@@ -208,7 +244,6 @@ def switch():
         time.sleep(0.5)
         if status == "Success":
             on_button.config(image = off)
-            
     else:
         status = subprocess.getoutput("warp-cli connect")
         time.sleep(1)
@@ -220,6 +255,7 @@ def switch():
     info_label.config(text = "", 
                 fg = "Black")
     threading.Thread(target=change_ip_text).start()
+
 
 # Create A Button
 if get_status()==True:
@@ -239,7 +275,7 @@ if get_status()==True:
         font = ("Arial", 15,'bold'))
 
 
-    
+
 else:
     my_label = Label(root, 
         text = "Disconnected", 
@@ -256,6 +292,7 @@ class TestThreading(object):
         thread = threading.Thread(target=self.run, args=(acc_label,))
         thread.daemon = True
         thread.start()
+
     def run(self,acc_label):
         pre_latency = ""
         pre_loss = ""
@@ -279,7 +316,7 @@ class TestThreading(object):
                     my_label_2.pack(padx=0, pady=0)
                 pre_latency = latency
                 pre_loss = loss
-
+ 
 
             if get_acc_type()==True:
                 acc_label.config(
@@ -303,15 +340,12 @@ class TestThreading(object):
                 my_label.config(text = "Connecting...", fg = "Black")
             time.sleep(self.interval)
 
-               
 
 tr = TestThreading()
 #logo
 # Define Our Images
 
-
 frame = Frame(root)
-
 frame.pack(side=BOTTOM)
 if (get_acc_type() == True):
     slogan = Button(frame, image = cflogo,
