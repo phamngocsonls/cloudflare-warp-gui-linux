132a4ec9c2b22e86ac44e59f3c03f4541613e3ab
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2024-08-16 13:45:01 +0200

    version updated to GUI v0.4
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- cf_teams/main.py
+++ cf_teams/main.py
@@ -207,7 +207,7 @@ root.geometry('360x480')
 root.iconphoto(False,appicon)
 root.config(bg = bgcolor)
 
-lbl = Label(root, text = "GUI v0.3", fg = "DimGray", bg = bgcolor,
+lbl = Label(root, text = "GUI v0.4", fg = "DimGray", bg = bgcolor,
     font = ("Arial", 12), pady=10, padx=10)
 lbl.grid()
 lbl.place(relx=0.0, rely=1.0, anchor='sw')
